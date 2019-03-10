c8ad0089d59f463ba8baa4108f2e2ca3b6e8a6a2
Forrest Pieper <forrestp@digitalalloys.com>
2019-03-10 01:07:03 -0500

    wip jupyterhub setup

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -3,37 +3,18 @@ c.JupyterHub.admin_access = True
 c.Spawner.default_url = '/lab'
 
 ## Authenticator
-from oauthenticator.oauth2 import OAuthLoginHandler
-from oauthenticator.generic import GenericOAuthenticator
-from tornado.auth import OAuth2Mixin
-
-class UVSQMixin(OAuth2Mixin):
-    _OAUTH_AUTHORIZE_URL = 'https://jupyter.ens.uvsq.fr/c2o2b/login'
-    _OAUTH_ACCESS_TOKEN_URL = 'https://jupyter.ens.uvsq.fr/c2o2b/token'
-
-class UVSQLoginHandler(OAuthLoginHandler, UVSQMixin):
-    pass
-
-class UVSQAuthenticator(GenericOAuthenticator):
-    login_service = 'UVSQ'
-    login_handler = UVSQLoginHandler
-    client_id = '0'
-    client_secret = ''
-    userdata_url = 'https://jupyter.ens.uvsq.fr/c2o2b/userdata'
-    token_url = 'https://jupyter.ens.uvsq.fr/c2o2b/token'
-    oauth_callback_url = 'https://jupyter.ens.uvsq.fr/hub/oauth_callback'
-
-c.JupyterHub.authenticator_class = UVSQAuthenticator
-
-c.Authenticator.admin_users = { 'lucadefe' }
-
+from oauthenticator.google import GoogleOAuthenticator
+c.JupyterHub.authenticator_class = GoogleOAuthenticator
 
 ## Docker spawner
 import os
+import dockerspawner
 
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
-c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+network_name = os.environ['DOCKER_NETWORK_NAME']
+c.DockerSpawner.network_name = network_name
+c.DockerSpawner.use_internal_ip = True
 # See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
 c.JupyterHub.hub_ip = os.environ['HUB_IP']
 
@@ -41,6 +22,7 @@ c.JupyterHub.hub_ip = os.environ['HUB_IP']
 # see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
 notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan'
 c.DockerSpawner.notebook_dir = notebook_dir
+c.DockerSpawner.format_volume_name = dockerspawner.volumenamingstrategy.escaped_format_volume_name
 c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
 
 # Other stuff
